9b42c1670e568312b1dd3eb813620560dbc65673
MedMoute <mehdi.ennaime@gmail.com>
2026-08-31 23:56:22 +0200

    Add Godot-compatible test framework & prooject

---
+++ UnitTests4Godot/test/TestGodotRuntimeWorks.cs
@@ -0,0 +1,15 @@
+﻿using GdUnit4;
+
+namespace UnitTests4Godot;
+
+//Dummy class ensuring the unit/integration tests requiring GodotRuntime
+// have a chance to pass
+[TestSuite]
+public sealed class TestGodotRuntimeWorks
+{
+    [TestCase]
+    [RequireGodotRuntime]
+    public void DummyTest()
+    {
+    }
+}
---
+++ UnitTests4Godot/test/TestSaveActions.cs
@@ -0,0 +1,14 @@
+using GdUnit4;
+
+namespace UnitTests4Godot;
+
+[TestSuite]
+public class TestSaveActions
+{
+    [TestCase]
+    public void TestSaveAction()
+    {
+     //   var terrain =  null
+    }
+    
+}
